e7841f3e76d58f96253a7bce89080627f10ac45d
Max Korop <makskorop99@gmail.com>
2024-10-26 20:39:23 +0300

    configuration: add usage of .env, set up running port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { type ConfigEnv, defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
+  const { VITE_PORT } = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+      port: Number(VITE_PORT),
+    },
+  });
+};
+
+export default config;
