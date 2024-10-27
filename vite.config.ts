719b2ec62e47c61fc7f4156c4551cbb1d17f92ed
Max Korop <makskorop99@gmail.com>
2024-10-27 22:16:13 +0200

    feat: add images, add packages, add type declaration, add import alias, add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { type ConfigEnv, defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import svgr from "vite-plugin-svgr";
 
 const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
   const { VITE_PORT } = loadEnv(mode, process.cwd());
@@ -8,9 +10,14 @@ const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
     server: {
       port: Number(VITE_PORT),
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
     },
   });
 };
