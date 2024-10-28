326f610a23767d5505c2711b65592650e264eeda
Max Korop <makskorop99@gmail.com>
2024-10-28 18:06:30 +0200

    add not-found, welcome pages

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,8 +1,16 @@
+import { ChadHeader } from "~/libs/components/components";
+import styles from "./styles.module.css"
+
 const NotFound: React.FC = () => {
   return (
-    <>
-      Not Found
-    </>
+    <div className={styles["card-wrapper"]}>
+      <div className={styles["card"]}>
+        <ChadHeader/>
+        <div className={styles["text"]}>
+          <span className={styles["header"]}>This page not found!</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -1,8 +1,17 @@
+import { ChadHeader } from "~/libs/components/components";
+import styles from "./styles.module.css";
+
 const WelcomePage: React.FC = () => {
   return (
-    <>
-      Welcome
-    </>
+    <div className={styles["card-wrapper"]}>
+      <div className={styles["card"]}>
+        <ChadHeader/>
+        <div className={styles["text"]}>
+          <span className={styles["header"]}>Hello Luna Edge, </span>
+          <span className={styles["header"]}>My name is [your name].</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
